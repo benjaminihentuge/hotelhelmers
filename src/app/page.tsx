06cf356cf6d050e2ee562768c3bfbde80e9f415b
benjaminihentuge <benjaminihentuge2017@gmail.com>
2024-12-25 00:09:08 +0100

    now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,21 @@
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // Import FontAwesome styles
+import './globals.css'; // Import global styles
+
+// Prevent FontAwesome from auto-adding CSS to the DOM
+config.autoAddCss = false;
 
 import Hero from "./components/Hero";
 import FeaturedRooms from "./components/FeaturedRooms";
 import Amenities from "./components/Amenities";
 import Contact from "./components/Contact";
 
-import './globals.css';
-
-
 const Home = () => (
   <>
-    
     <Hero />
     <FeaturedRooms />
     <Amenities />
     <Contact />
-    
-
-    
-    
   </>
 );
 
